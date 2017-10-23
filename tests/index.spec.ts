94a4110d5e6a4404cafb932d1d71b8c951a4b235
Onur Aykac <onuraykac@gmail.com>
2017-10-23 13:25:55 +0300

    Init ts and testing

---
+++ tests/index.spec.ts
@@ -0,0 +1,12 @@
+import { hello } from '../src/index';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Hello function', () => {
+
+  it('should return hello world', () => {
+    const result = hello();
+    expect(result).to.equal('Hello world!');
+  });
+
+});
